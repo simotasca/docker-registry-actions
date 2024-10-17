624fa9ab1c7a52dc4e4ac000767116320f42180a
simotasca <simo.tasca@gmail.com>
2024-10-17 18:23:36 +0200

    fixed install with user prompt

--- releases/linux/install.sh
+++ releases/linux/install.sh
@@ -5,6 +5,15 @@ BINARY_NAME="docker-registry-actions"
 GITHUB_URL="https://github.com/simotasca/docker-registry-actions/releases/$BINARY_NAME"
 SERVICE_NAME="docker-registry-actions"
 
+# Prompt user for arguments
+read -p "Enter path to compose file: " COMPOSE_PATH
+read -p "Enter service name list space separated (e.g., db backend website): " SERVICE_NAME_ARG
+read -p "Enter host (default: 0.0.0.0): " HOST
+HOST=${HOST:-0.0.0.0}
+read -p "Enter port (default: 4483): " PORT
+PORT=${PORT:-4483}
+read -p "Enter token secret: " TOKEN_SECRET
+
 # Download and install the binary
 sudo wget -O /usr/local/bin/$BINARY_NAME $GITHUB_URL
 sudo chmod +x /usr/local/bin/$BINARY_NAME
@@ -16,7 +25,7 @@ Description=Riavvia servizi docker compose quando vengono aggiornate le immagini
 After=network.target
 
 [Service]
-ExecStart=/usr/local/bin/$BINARY_NAME
+ExecStart=/usr/local/bin/$BINARY_NAME -c $COMPOSE_PATH $SERVICE_NAME_ARG --host $HOST --port $PORT -t $TOKEN_SECRET
 Restart=on-failure
 User=$(whoami)
 
@@ -31,3 +40,5 @@ sudo systemctl start $SERVICE_NAME
 
 # Confirm the service is running
 sudo systemctl status $SERVICE_NAME
+
+echo "Modify the configuration by editing /etc/systemd/system/$SERVICE_NAME.service"
